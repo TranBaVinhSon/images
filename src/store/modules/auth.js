2d9fb92741eff8cc6ec607ff967ed28c1f905fbd
Tran B.V. Son <tranbavinhson@gmail.com>
2018-05-19 00:01:51 +0700

    add stuff

---
+++ src/store/modules/auth.js
@@ -0,0 +1,21 @@
+const state = {
+  token: null
+};
+
+const getters = {
+  isLoggedIn: (state) => {
+    return !!state.token;
+  }
+};
+
+const actions = {
+  logout: ({ commit }) => {
+    commit("setToken", null);
+  }
+};
+
+const mutations = {
+  setToken: (state, token) => {
+    state.token = token;
+  }
+};
